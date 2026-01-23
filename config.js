715208309040c3bf244ddab741c811f709bb3f72
bayzzzzy <128719940+bayuxxd@users.noreply.github.com>
2026-01-23 20:00:29 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: "8049486838:AAHJ1tTtz-pXznJU5WV5MW7OYbyzjQf8na4",
+  BOT_TOKEN: "8049486838:AAGg5OpHG178Ctcn5qfK6QaJccKW7GKjm6g",
     allowedDevelopers: ['8578348316'], // ID
 };
